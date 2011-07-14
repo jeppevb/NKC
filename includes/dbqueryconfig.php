553fd89d370943118d6da2169567978ff4843cc4
Jeppe B <jeppe@kyomu.dk>
2011-07-14 11:30:16 +0200

    removing stuffs

--- includes/dbqueryconfig.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$qcon = mysql_connect("localhost:3306","query_uname","password");
-	if (!$qcon){
-		die('Could not connect: ' . mysql_error());
-	}
-	mysql_select_db("nkc", $qcon);
-	mysql_set_charset('utf8',$qcon);
-?>	
