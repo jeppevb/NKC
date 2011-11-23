7aa07a837a34d090c92264795b5cb44ef0f55f06
unknown <jeppe@misato.(none)>
2011-11-23 21:17:14 +0100

    escapes and more

---
+++ foto.php
@@ -0,0 +1,38 @@
+<?php include_once 'includes/header.php';?>
+<?php include_once 'includes/ads.php';?>
+<?php include_once 'includes/galleryhelper.php';?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link href="/stylesheets/stylesheet.css" media="screen" rel="stylesheet"
+	type="text/css" />
+<link rel="icon" type="image/icon" href="/favicon.ico" />
+<?php ads_js_include(); ?>
+<?php analytics_js(); ?>
+<title>Nyheder fra Nordjysk Kampsportscenter</title>
+<meta http-equiv="description" content="Her kan du se billeder fra Nordjysk Kampsportscenter." />
+</head>
+<body onload="setAd();">
+	<div class="thestyle" id="top">
+	<?php show_topbanner(); ?>
+	</div>
+	<div id="menu" class="thestyle">
+		<!-- Her begynder menuen  -->
+		<?php show_menu(); ?>
+		<!-- Her ender menuen  -->
+	</div>
+	<div class="thestyle" id="whitebox">
+		<div id="content">
+			<?php 
+			if(isset($_GET['id'])){
+				gallery($_GET['id']);
+			}else{
+				fetch_galleries();
+			}
+			?>
+		</div>
+	</div>
+	<div id="footer" class="thestyle"></div>
+</body>
+</html>
